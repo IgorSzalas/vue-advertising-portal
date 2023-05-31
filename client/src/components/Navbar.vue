94125d69907bc90cdccb2f3583d671229f670d18
Igor Szalas <igorszalas@int.pl>
2023-05-31 21:14:06 +0200

    Add navbar structure

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header id="navbar">
+    <div
+      id="page-logo"
+      src="https://www.google.com/url?sa=i&url=https%3A%2F%2Frootblog.pl%2Fyoutube-nowe-logo%2F&psig=AOvVaw1NC2T0fbkbgsw3JxQRDTnm&ust=1685643243627000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPCSlceUoP8CFQAAAAAdAAAAABAJ"
+    ></div>
+    <nav id="links-container">
+      <RouterLink class="links" to="/">Strona główna</RouterLink>
+      <RouterLink class="links" to="/login">Zaloguj się</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+#navbar {
+  background-color: #464646;
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+}
+
+#page-logo {
+  height: 50px;
+  width: 50px;
+  background-color: red;
+}
+
+#links-container {
+  display: inline;
+  padding: 15px;
+}
+
+.links {
+  display: inline;
+  margin: 20px;
+  text-decoration: none;
+  color: #ffffff;
+}
+
+.links:hover {
+  color: #9a9a9a;
+}
+</style>
